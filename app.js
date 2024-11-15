4de140a7872913688afdefb1e65d1e2e815eb82c
Naitik khoriya <naitikkhoriya144263@gmail.com>
2024-11-15 22:19:48 +0530

    setup complete

---
+++ app.js
@@ -0,0 +1,35 @@
+var canvas = document.querySelector("canvas");
+var context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  render();
+});
+
+function render() {
+  drawImageScaled(images[airpods.frame], context);
+}
+function drawImageScaled(img, context) {
+  var canvas = context.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.min(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+}
